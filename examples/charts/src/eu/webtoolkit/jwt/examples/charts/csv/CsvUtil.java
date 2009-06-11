f0c7213bacbbc45e76a13c799132ef69251e601b
Pieter Libin <pieter@localhost.localdomain>
2009-06-11 18:24:56 +0200

    remove unnecessary imports

--- examples/charts/src/eu/webtoolkit/jwt/examples/charts/csv/CsvUtil.java
+++ examples/charts/src/eu/webtoolkit/jwt/examples/charts/csv/CsvUtil.java
@@ -7,8 +7,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import eu.webtoolkit.jwt.WAbstractItemModel;
 import eu.webtoolkit.jwt.WContainerWidget;
