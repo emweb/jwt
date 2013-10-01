d6feb89203c87a3131a4f7e53107d8991c61fe16
Koen Deforche <koen@emweb.be>
2013-10-01 09:10:32 +0200

    do not access dialogcover on shutdown

--- src/eu/webtoolkit/jwt/render/Css21Parser.java
+++ src/eu/webtoolkit/jwt/render/Css21Parser.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.5 src/eu/webtoolkit/jwt/render/Css21.g 2013-09-30 16:53:12
+// $ANTLR 3.5 src/eu/webtoolkit/jwt/render/Css21.g 2013-10-01 09:08:36
 package eu.webtoolkit.jwt.render;
 
 import org.antlr.runtime.*;
