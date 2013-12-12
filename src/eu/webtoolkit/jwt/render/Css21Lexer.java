034dd80a3ddcf98d0c5cbefbbf89961a4e8ada9d
Koen Deforche <koen@emweb.be>
2013-12-12 14:43:16 +0100

    minified that

--- src/eu/webtoolkit/jwt/render/Css21Lexer.java
+++ src/eu/webtoolkit/jwt/render/Css21Lexer.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.5 src/eu/webtoolkit/jwt/render/Css21.g 2013-12-11 16:23:12
+// $ANTLR 3.5 src/eu/webtoolkit/jwt/render/Css21.g 2013-12-12 14:40:56
 package eu.webtoolkit.jwt.render;
 
 import org.antlr.runtime.*;
--- src/eu/webtoolkit/jwt/render/Css21Parser.java
+++ src/eu/webtoolkit/jwt/render/Css21Parser.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.5 src/eu/webtoolkit/jwt/render/Css21.g 2013-12-11 16:23:11
+// $ANTLR 3.5 src/eu/webtoolkit/jwt/render/Css21.g 2013-12-12 14:40:55
 package eu.webtoolkit.jwt.render;
 
 import org.antlr.runtime.*;
