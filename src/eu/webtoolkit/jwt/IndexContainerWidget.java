9397d1535bb1b60b40a9458eabf843876a9a1ee5
Koen Deforche <koen@emweb.be>
2014-04-30 15:06:35 +0200

    added new files

---
+++ src/eu/webtoolkit/jwt/IndexContainerWidget.java
@@ -0,0 +1,22 @@
+package eu.webtoolkit.jwt;
+
+public class IndexContainerWidget extends WContainerWidget {
+	private WModelIndex index;
+
+	public IndexContainerWidget(WModelIndex index) {
+		this.index = index;
+	}
+
+	public void setIndex(WModelIndex index2) {
+		this.index = index2;
+	}
+
+	public WModelIndex getIndex() {
+		return index;
+	}
+
+	public WString getToolTip(){
+		return StringUtils.asString(
+			  index.getData(ItemDataRole.ToolTipRole));
+	}
+}
---
+++ src/eu/webtoolkit/jwt/IndexText.java
@@ -0,0 +1,22 @@
+package eu.webtoolkit.jwt;
+
+public class IndexText  extends WText {
+	private WModelIndex index;
+
+	public IndexText(WModelIndex index) {
+		this.index = index;
+	}
+
+	public void setIndex(WModelIndex index2) {
+		this.index = index2;
+	}
+
+	public WModelIndex getIndex() {
+		return index;
+	}
+
+	public WString getToolTip(){
+		return StringUtils.asString(
+			  index.getData(ItemDataRole.ToolTipRole));
+	}
+}
