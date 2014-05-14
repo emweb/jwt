72bff30306d44081af9b6198a678eb76586b56c2
Koen Deforche <koen@emweb.be>
2014-05-14 10:37:08 +0200

    version bump

--- src/eu/webtoolkit/jwt/render/Css21Parser.java
+++ src/eu/webtoolkit/jwt/render/Css21Parser.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.5 src/eu/webtoolkit/jwt/render/Css21.g 2014-05-14 10:25:19
+// $ANTLR 3.5 src/eu/webtoolkit/jwt/render/Css21.g 2014-05-14 10:33:53
 package eu.webtoolkit.jwt.render;
 
 import org.antlr.runtime.*;
